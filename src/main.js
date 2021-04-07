78a648f359954eb596e7ee0c20116feeeacf1f96
Danny Baghdasarians <dbaghdas@ucsc.edu>
2021-04-07 13:52:57 -0700

    Initial Commit. Blank Blank 640x480

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//
+//python -m http.server each time you start VSCODE on terminal
+//url: localhost:8000
+//
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+}
+
+let game = new Phaser.Game(config);
