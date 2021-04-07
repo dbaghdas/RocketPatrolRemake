0b180edac1d0f75c5a40f862329d146e1822950b
Danny Baghdasarians <dbaghdas@ucsc.edu>
2021-04-07 14:24:41 -0700

    Added assets and added starfield background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,14 +3,21 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+    }
+
     create() {
+        // Set Starfield Background
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+    
         // Green UI Background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-
+        
+        // White UI Borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
     }
 }
