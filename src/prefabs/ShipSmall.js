38daa5c359727bdc86d6bf8d2948db1edf0c6e00
Danny Baghdasarians <dbaghdas@ucsc.edu>
2021-04-15 13:37:43 -0700

    Added Background music and new ship type

---
+++ src/prefabs/ShipSmall.js
@@ -0,0 +1,19 @@
+class ShipSmall extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+    }
+
+    update() {
+        this.x -= game.settings.spaceshipSpeed * 1.8;
+
+        if(this.x < -this.width) {
+            this.x = game.config.width;
+        }
+    }
+
+    reset() {
+        this.x = game.config.width + 50;
+        this.alpha = 1;
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,10 +10,13 @@ class Play extends Phaser.Scene {
         this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         this.load.audio('sfx_explosion', 'assets/explosion38.wav');
         this.load.audio('sfx_rocket', 'assets/rocket_shot.wav');
+        this.load.audio('backgroundMusic', 'assets/nanophage.mp3');
+        this.load.image('smallspaceship', 'assets/spaceshipSmall.png');
     }
 
     create() {
-
+        // Play background music
+        this.sound.play('backgroundMusic');
         // Set Starfield Background
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
@@ -25,6 +28,7 @@ class Play extends Phaser.Scene {
         this.ship1 = new Ship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship2 = new Ship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship3 = new Ship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+        this.ship4 = new ShipSmall(this, game.config.width, borderUISize*7 + borderPadding*4, 'smallspaceship', 0, 10).setOrigin(0,0);
 
         // Green UI Background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
@@ -112,16 +116,20 @@ class Play extends Phaser.Scene {
         this.ship1.update();
         this.ship2.update();
         this.ship3.update();
+        this.ship4.update();
         
         if(this.checkCollision(this.p1Rocket, this.ship3)) {
             this.shipExplode(this.p1Rocket, this.ship3);  
-          }
+        }
         if (this.checkCollision(this.p1Rocket, this.ship2)) {
             this.shipExplode(this.p1Rocket, this.ship2);  
-          }
+        }
         if (this.checkCollision(this.p1Rocket, this.ship1)) {
             this.shipExplode(this.p1Rocket, this.ship1);  
-          }
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship4)) {
+            this.shipExplode(this.p1Rocket, this.ship4);  
+        }
         if (this.checkCollision(this.p2Rocket, this.ship3)) {
             this.shipExplode(this.p2Rocket, this.ship3); 
         }
@@ -131,6 +139,9 @@ class Play extends Phaser.Scene {
         if (this.checkCollision(this.p2Rocket, this.ship1)) {
             this.shipExplode(this.p2Rocket, this.ship1); 
         }
+        if (this.checkCollision(this.p2Rocket, this.ship4)) {
+            this.shipExplode(this.p2Rocket, this.ship4); 
+        }
 
         if (!this.gameOver) {               
             this.p1Rocket.update();  
@@ -138,6 +149,7 @@ class Play extends Phaser.Scene {
             this.ship1.update();           // update spaceships (x3)
             this.ship2.update();
             this.ship3.update();
+            this.ship4.update();
         } 
 
           // check key input for restart
@@ -177,7 +189,13 @@ class Play extends Phaser.Scene {
         // score add and repaint
         this.sound.play('sfx_explosion');
 
-        ship.points = 1;
+        if(ship == this.ship4)
+        {
+            ship.points = 3;
+        }
+        else {
+            ship.points = 1;
+        }
         if(rocket == this.p1Rocket) {
             this.p1Score += ship.points;
             this.scoreLeft.text = this.p1Score;  
